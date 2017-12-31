c0755756ad9580762afca802bf94e1caba783c84
Makoto Chiba <hypermkt@gmail.com>
2018-01-01 01:22:48 +0900

    Add ServiceProvider

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        \SocialiteProviders\Manager\ServiceProvider::class,
 
         /*
          * Package Service Providers...
