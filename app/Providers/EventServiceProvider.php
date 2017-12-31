8f19bc5db3a0a075df9bcb098df2e0c3dde8ea58
Makoto Chiba <hypermkt@gmail.com>
2018-01-01 01:23:10 +0900

    Add event and listners

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -16,6 +16,9 @@ class EventServiceProvider extends ServiceProvider
         'App\Events\Event' => [
             'App\Listeners\EventListener',
         ],
+        \SocialiteProviders\Manager\SocialiteWasCalled::class => [
+            'SocialiteProviders\Slack\SlackExtendSocialite@handle',
+        ],
     ];
 
     /**
