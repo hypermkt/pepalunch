7a414e9aa6577a0018b1d0b79890a822c05d2a68
Makoto Chiba <hypermkt@gmail.com>
2018-01-04 23:10:16 +0900

    テストケースを追加

--- tests/Feature/LunchTest.php
+++ tests/Feature/LunchTest.php
@@ -8,9 +8,23 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class LunchTest extends TestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->artisan('migrate:fresh');
+        $this->artisan('db:seed');
+
+    }
     public function testLunchIndex()
     {
-        $this->json('GET', '/api/lunches')
-            ->assertStatus(200);
+        $response = $this->call('GET', '/api/lunches');
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $data = $response->getData(true);
+        $this->assertArrayHasKey('id', $data[0]);
+        $this->assertArrayHasKey('lunch_at', $data[0]);
+        $this->assertArrayHasKey('id', $data[0]['users'][0]);
+        $this->assertArrayHasKey('name', $data[0]['users'][0]);
     }
 }
