4aa8fdb9db0ecb17c742c6b18b863e1c5cd88b59
Makoto Chiba <hypermkt@gmail.com>
2018-01-07 17:40:03 +0900

    SlackとOAuth認証して、JWTを発行するエントリポイントを追加

--- app/Http/Controllers/Auth/SlackController.php
+++ app/Http/Controllers/Auth/SlackController.php
@@ -7,33 +7,39 @@ use App\Http\Controllers\Controller;
 use App\User;
 use Auth;
 use Socialite;
+use Tymon\JWTAuth\JWTAuth;
 
 class SlackController extends Controller
 {
+    protected $jwt;
+
+    public function __construct(JWTAuth $jwt)
+    {
+        $this->jwt = $jwt;
+    }
+
     public function redirectToSlackProvider()
     {
-        return Socialite::with('slack')->redirect();
+        return Socialite::with('slack')->stateless()->redirect();
     }
 
     public function logout()
     {
-        Auth::logout();
-
-        return redirect('/');
+        // TODO
     }
 
     public function handleSlackProviderCallback()
     {
        try {
-            $slackUser = Socialite::driver('slack')->user();
-            $authUser = $this->findOrCreateUser($slackUser);
+            $slackUser = Socialite::driver('slack')->stateless()->user();
+            $user = $this->findOrCreateUser($slackUser);
 
-            Auth::login($authUser);
+            $token = $this->jwt->fromUser($user);
 
+           return response()->json(compact('authUser', 'token'));
        } catch (Exception $e) {
 
        }
-       return redirect('/');
     }
 
     private function findOrCreateUser($slackUser) {
