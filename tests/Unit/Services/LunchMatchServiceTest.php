4ddf31335973b87d6a7cce90331f04d3327449d6
Makoto Chiba <hypermkt@gmail.com>
2018-01-08 17:46:14 +0900

    リファクタリング インスタンスをコンストラクタで実施

--- tests/Unit/Services/LunchMatchServiceTest.php
+++ tests/Unit/Services/LunchMatchServiceTest.php
@@ -8,31 +8,32 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class LunchMatchServiceTest extends TestCase
 {
+    protected $service;
+
     public function setUp()
     {
         parent::setUp();
 
         $this->artisan('migrate:fresh');
+        $this->service = new LunchMatchService();
     }
 
     public function testCalculateBaseDate_WhenBefore13()
     {
         $testDate = Carbon::create(2018, 1, 1, 12, 59);
         Carbon::setTestNow($testDate);
-        $service = new LunchMatchService();
-        $this->assertEquals(1, $service->calculateBaseDate()->day);
-        $this->assertEquals(13, $service->calculateBaseDate()->hour);
-        $this->assertEquals(0, $service->calculateBaseDate()->minute);
+        $this->assertEquals(1, $this->service->calculateBaseDate()->day);
+        $this->assertEquals(13, $this->service->calculateBaseDate()->hour);
+        $this->assertEquals(0, $this->service->calculateBaseDate()->minute);
     }
 
     public function testCalculateBaseDate_WhenAfter13()
     {
         $testDate = Carbon::create(2018, 1, 1, 13, 0);
         Carbon::setTestNow($testDate);
-        $service = new LunchMatchService();
-        $this->assertEquals(2, $service->calculateBaseDate()->day);
-        $this->assertEquals(13, $service->calculateBaseDate()->hour);
-        $this->assertEquals(0, $service->calculateBaseDate()->minute);
+        $this->assertEquals(2, $this->service->calculateBaseDate()->day);
+        $this->assertEquals(13, $this->service->calculateBaseDate()->hour);
+        $this->assertEquals(0, $this->service->calculateBaseDate()->minute);
     }
 
     public function testGetCandidateDates()
@@ -40,7 +41,6 @@ class LunchMatchServiceTest extends TestCase
         $now = Carbon::now()->setDate(2018, 1, 1)->setTime(13, 0);
         $now1 = clone $now;
         $now2 = clone $now;
-        $service = new LunchMatchService();
         $this->assertEquals([
             clone $now1->setDate(2018, 1, 2),
             clone $now1->setDate(2018, 1, 3),
@@ -50,7 +50,7 @@ class LunchMatchServiceTest extends TestCase
             clone $now1->setDate(2018, 1, 9),
             clone $now1->setDate(2018, 1, 10),
             clone $now1->setDate(2018, 1, 11),
-        ], $service->getCandidateDates(1, $now2));
+        ], $this->service->getCandidateDates(1, $now2));
     }
 
     public function testGetCandidates_ReturnFalse_WhenUserWasAlone()
@@ -58,10 +58,9 @@ class LunchMatchServiceTest extends TestCase
         factory(App\User::class, 1)->create();
         $testDate = Carbon::create(2018, 1, 1, 12, 59);
         Carbon::setTestNow($testDate);
-        $service = new LunchMatchService();
-        $candidateDates = $service->getCandidateDates(1, $service->calculateBaseDate());
+        $candidateDates = $this->service->getCandidateDates(1, $this->service->calculateBaseDate());
 
-        $this->assertFalse($service->getCandidates(1, $candidateDates));
+        $this->assertFalse($this->service->getCandidates(1, $candidateDates));
     }
 
     public function testGetCandidates_WhenUserWasFivePeople()
@@ -69,13 +68,12 @@ class LunchMatchServiceTest extends TestCase
         factory(App\User::class, 5)->create();
         $testDate = Carbon::create(2018, 1, 1, 12, 59);
         Carbon::setTestNow($testDate);
-        $service = new LunchMatchService();
-        $candidateDates = $service->getCandidateDates(1, $service->calculateBaseDate());
+        $candidateDates = $this->service->getCandidateDates(1, $this->service->calculateBaseDate());
 
         $this->assertEquals(
             Carbon::now()->setDate(2018, 1, 2)->setTime(13, 0),
-            $service->getCandidates(1, $candidateDates)['date']
+            $this->service->getCandidates(1, $candidateDates)['date']
         );
-        $this->assertCount(3, $service->getCandidates(1, $candidateDates)['candidates']);
+        $this->assertCount(3, $this->service->getCandidates(1, $candidateDates)['candidates']);
     }
 }
