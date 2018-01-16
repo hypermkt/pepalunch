ceb7eb160689eb709c044a66f2b70c4f43d3bf80
Makoto Chiba <hypermkt@gmail.com>
2018-01-17 00:21:30 +0900

    現在のランチに行きたい状態をSwitchボタンに反映

--- resources/assets/js/components/ListLunch.vue
+++ resources/assets/js/components/ListLunch.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
+        <p>ランチに行きたい：<el-switch v-model="active"></el-switch></p>
+        <br />
         <button @click="createShuffleLunch()">シャッフルランチ</button>
         <h2>ランチ予定</h2>
         <template v-for="lunch in lunches">
@@ -15,20 +17,34 @@
 
 <script>
     import api from '../api/index';
+    import jwtDecode from 'jwt-decode';
 
     export default {
         data() {
             return {
+                user: {},
                 lunches: []
             }
         },
+        computed: {
+            active() {
+                return !!this.user.active;
+            }
+        },
         mounted() {
+            this.fetchUser();
             this.fetchMyLunches();
         },
         methods: {
             fetchMyLunches() {
                 api.lunch.list().then((response) => {
                     this.lunches = response.data;
+                });
+            },
+            fetchUser() {
+                let payload = jwtDecode(localStorage.getItem('token'));
+                api.user.show(payload.sub).then((response) => {
+                    this.user = response.data;
                 });
             },
             createShuffleLunch() {
